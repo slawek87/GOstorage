ed81cca1014c0cb110dd7b42ef85515d549f46a1
Sławomir Kabik <slawek@redsoftware.pl>
2017-09-13 19:54:21 +0200

    refactoring storage

--- storage/utils.go
+++ storage/utils.go
@@ -1,62 +1,12 @@
 package storage
 
 import (
-	"mime/multipart"
-	"io"
-	"os"
-	"github.com/slawek87/GOstorage/settings"
 	"strings"
 	"errors"
 	"encoding/base64"
-	"strconv"
 )
 
-func (storage *Storage) IsFileExists(filename string) bool {
-	if _, err := os.Stat(filename); err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-	}
-	return true
-}
-
-func (storage *Storage) GetPath(filename string, token string) string {
-	return settings.FILE_STORAGE + token + "/" + filename
-}
-
-func (storage *Storage) GetFilename(filename string, token string, counter int) string {
-	strCounter := counter
-
-	if counter > 0 {
-		filename = strconv.Itoa(strCounter) + "_" + filename
-	}
-
-	path := storage.GetPath(filename, token)
-
-	if storage.IsFileExists(path) == true {
-		counter++
-		return storage.GetFilename(filename, token, counter)
-	}
-
-	return filename
-}
-
-func (storage *Storage) SaveFile(file multipart.File, header *multipart.FileHeader, token string) (string, error) {
-    filename := storage.GetFilename(header.Filename, token, 0)
-    path := storage.GetPath(filename, token)
-
-	out, err := os.Create(path)
-
-	if err != nil {
-		return header.Filename, err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, file)
-
-	return filename, err
-}
-
+// method decodes Authorisation token. Each token include username and password.
 func DecodeToken(token string) (string, string, error) {
 	splitToken := strings.Split(token, " ")
 
